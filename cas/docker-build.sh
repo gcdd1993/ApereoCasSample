1efc151fc75c829ecfbfb20e16a0ce90218c2db2
gcdd1993 <gcwm99@gmail.com>
2021-12-24 18:35:18 +0800

    修改为自己的dockerhub

--- cas/docker-build.sh
+++ cas/docker-build.sh
@@ -3,13 +3,13 @@
 imageTag="$1"
 
 if [ -z "$imageTag" ]; then
-  version=(`cat gradle.properties | grep "cas.version" | cut -d= -f2`)
-  imageTag="v$version" 
+  version=($(cat gradle.properties | grep "cas.version" | cut -d= -f2))
+  imageTag="v$version"
 fi
 
 echo "Building CAS docker image tagged as [$imageTag]"
 # read -p "Press [Enter] to continue..." any_key;
 
-docker build --tag="apereo/cas:$imageTag" . \
-  && echo "Built CAS image successfully tagged as apereo/cas:$imageTag" \
-  && docker images "apereo/cas:$imageTag"
+docker build --tag="gcdd1993/cas:$imageTag" . &&
+  echo "Built CAS image successfully tagged as apereo/cas:$imageTag" &&
+  docker images "apereo/cas:$imageTag"
